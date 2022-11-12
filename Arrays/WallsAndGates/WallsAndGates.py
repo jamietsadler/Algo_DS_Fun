6c21fbc2919e5b8d24717e2f9e3309fa1f1d2386
Jamie Sadler <jamietsadler@hotmail.co.uk>
2022-11-12 18:48:56 +0300

    Walls and Gates

---
+++ Arrays/WallsAndGates/WallsAndGates.py
@@ -0,0 +1,26 @@
+import collections
+
+class Solution:
+    def wallsAndGates(self, rooms):
+        if not rooms:
+            return 
+        r, c= len(rooms), len(rooms[0])
+        for i in range(r):
+            for j in range(c):
+                if rooms[i][j] == 0:
+                    queue = collections.deque([])
+                    queue.append((i+1, j, 1))
+                    queue.append((i-1, j, 1))
+                    queue.append((i, j+1, 1))
+                    queue.append((i, j-1, 1))
+                    visited = set()
+                    while queue:
+                        x, y, val = queue.popleft()
+                        if x < 0 or x >= r or y < 0 or y >= c or rooms[x][y] in [0, -1] or (x, y) in visited:
+                            continue
+                        visited.add((x, y))
+                        rooms[x][y] = min(rooms[x][y], val)
+                        queue.append((x+1, y, val+1))
+                        queue.append((x-1, y, val+1))
+                        queue.append((x, y+1, val+1))
+                        queue.append((x, y-1, val+1))
