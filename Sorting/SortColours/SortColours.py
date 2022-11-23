314914bb35957636db7ad780ceb48471a828b2a3
Jamie Sadler <jamietsadler@hotmail.co.uk>
2022-11-23 18:06:08 +0300

    sort colours using counting sort

--- Sorting/SortColours/SortColours.py
+++ Sorting/SortColours/SortColours.py
@@ -19,3 +19,35 @@ class Solution:
                 p2 -= 1
             else:
                 curr += 1
+
+
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        K = max(nums)
+        counts = [0] * (K + 1)
+        for elem in nums:
+            counts[elem] += 1
+        print(counts)
+        starting_index = 0
+        for i, count in enumerate(counts):
+            counts[i] = starting_index
+            starting_index += count
+        print(counts)
+        
+        sorted_lst = [0] * len(nums)
+
+        for elem in nums:
+            sorted_lst[counts[elem]] = elem
+            # since we have placed an item in index counts[elem], we need to
+            # increment counts[elem] index by 1 so the next duplicate element
+            # is placed in appropriate index
+            counts[elem] += 1
+        print(counts)
+        print(sorted_lst)
+        for i in range(len(nums)):
+            nums[i] = sorted_lst[i]
+        
