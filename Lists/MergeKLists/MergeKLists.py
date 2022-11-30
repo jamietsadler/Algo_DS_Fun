b37041c05dbffdd65162a9808918c441d1a17845
Jamie Sadler <jamietsadler@hotmail.co.uk>
2022-11-30 20:59:25 +0300

    Merge K Lists Divide & Conquer

--- Lists/MergeKLists/MergeKLists.py
+++ Lists/MergeKLists/MergeKLists.py
@@ -1,4 +1,4 @@
-from typing import ListNode
+from typing import ListNode, List, Optional
 # Definition for singly-linked list.
 # class ListNode(object):
 #     def __init__(self, val=0, next=None):
@@ -38,3 +38,38 @@ class Solution(object):
         else:
             curr.next= l1
         return dummy.next
+
+
+# Divide and Conquer
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        length = len(lists)
+        interval = 1
+        
+        while interval < length:
+            for i in range(0, length - interval, interval*2):
+                lists[i] = self.mergeLists(lists[i], lists[i + interval])
+            interval *= 2
+        return lists[0] if length > 0 else None
+        
+    def mergeLists(self, l1, l2):
+        dummy = curr = ListNode(0)
+        while l1 and l2:
+            if l1.val < l2.val:
+                curr.next = l1
+                l1 = l1.next
+            else:
+                curr.next = l2
+                l2 = l2.next
+            curr = curr.next
+        if not l1:
+            curr.next = l2
+        else:
+            curr.next= l1
+        return dummy.next
+    
