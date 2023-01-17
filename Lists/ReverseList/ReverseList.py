ff4da494e79cbf5ea45fa651b6a0f7c60ee01c5f
Jamie Sadler <jamietsadler@hotmail.co.uk>
2023-01-17 15:47:53 +0000

    Reverse List iterative

--- Lists/ReverseList/ReverseList.py
+++ Lists/ReverseList/ReverseList.py
@@ -1,3 +1,5 @@
+from typing import Optional, ListNode
+
 class Solution:
     def reverseList(self, head):
         if (not head) or (not head.next):
@@ -7,3 +9,16 @@ class Solution:
         head.next.next = head
         head.next = None
         return p
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        curr = head
+        
+        while curr:
+            next_temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_temp
+        
+        return prev
