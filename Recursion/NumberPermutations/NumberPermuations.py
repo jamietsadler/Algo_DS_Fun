1b2c962f21a78ed23cb30046a2066f3a08b84a71
Jamie Sadler <jamietsadler@hotmail.co.uk>
2023-01-24 15:15:23 +0300

    Number Permutations using backtracking

---
+++ Recursion/NumberPermutations/NumberPermuations.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        n = len(nums)
+        def backtrack(first = 0):
+            if first == n:
+                output.append(nums[:])
+            for i in range(first, n):
+                nums[first], nums[i] = nums[i], nums[first]
+                
+                backtrack(first+1)
+                
+                nums[first], nums[i] = nums[i], nums[first]
+                
+        output = []
+        backtrack()
+        
+        return output
