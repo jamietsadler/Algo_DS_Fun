9a5bf07c4ba7720da484b2bf34fc0ecd937da40f
Jamie Sadler <jamietsadler@hotmail.co.uk>
2023-03-14 18:35:45 +0000

    Invert binary tree

---
+++ Trees/InvertBinaryTree/InvertBinaryTree.py
@@ -0,0 +1,34 @@
+from typing import Optional, TreeNode
+
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if not root:
+            return
+        right = self.invertTree(root.right)
+        left = self.invertTree(root.left)
+        root.right = left
+        root.left = right
+
+        return root
+    
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        queue = deque([root])
+
+        while queue:
+            curr = queue.popleft()
+            curr.left, curr.right = curr.right, curr.left
+
+            if curr.left:
+                queue.append(curr.left)
+            if curr.right:
+                queue.append(curr.right)
+
+        return root
