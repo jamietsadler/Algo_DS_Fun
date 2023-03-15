3e620867107e4cd9039b828ea4dd04831f2c24d8
Jamie Sadler <jamietsadler@hotmail.co.uk>
2023-03-15 14:38:36 +0000

    Coin change bottom-up dp

--- DynamicProgramming/CoinChange/CoinChange.py
+++ DynamicProgramming/CoinChange/CoinChange.py
@@ -18,3 +18,19 @@ class Solution:
             return min_cost if min_cost != float('inf') else -1
             
         return dfs(amount)
+
+
+class Solution(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        dp = [float("inf")] * (amount + 1)
+
+        dp[0] = 0
+        for coin in coins:
+            for x in range(coin, amount + 1):
+                dp[x] = min(dp[x], dp[x - coin] + 1)
+        return dp[amount] if dp[amount] != float("inf") else -1
