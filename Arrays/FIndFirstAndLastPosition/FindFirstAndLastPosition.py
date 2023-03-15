18a8ebd2212c1800b12a5ac818d3bfbf6295c192
Jamie Sadler <jamietsadler@hotmail.co.uk>
2023-03-15 14:43:29 +0000

    Find first and last position of element in array

---
+++ Arrays/FIndFirstAndLastPosition/FindFirstAndLastPosition.py
@@ -0,0 +1,37 @@
+
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        if not nums:
+            return [-1, -1]
+        
+        def searchLow(nums, target):
+            lo, hi = 0, len(nums) - 1
+            while lo <= hi:
+                mid = (hi + lo)//2
+                if nums[mid] >= target:
+                    hi = mid - 1
+                else:
+                    lo = mid + 1
+            return lo
+        
+        def searchHigh(nums, target):
+            lo, hi = 0, len(nums) - 1
+            while lo <= hi:
+                mid = (lo + hi) // 2
+                if nums[mid] > target:
+                    hi = mid - 1
+                else:
+                    lo = mid + 1
+            return hi
+        
+        lo = searchLow(nums, target)
+        hi = searchHigh(nums, target)
+        if len(nums) >= lo >= 0  and lo <= hi and nums[lo] == target:
+            return [lo, hi]
+        
+        return [-1, -1]
