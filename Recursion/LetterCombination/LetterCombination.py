35930b9de6c4562b88dc2c70e1d6090534643fed
Jamie Sadler <jamietsadler@hotmail.co.uk>
2023-03-22 16:49:24 +0000

    Letter Combinations

--- Recursion/LetterCombination/LetterCombination.py
+++ Recursion/LetterCombination/LetterCombination.py
@@ -26,3 +26,35 @@ class Solution:
         backtrack([], 0)
         return ans
                 
+
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        mappings = {
+            '2': ['a', 'b', 'c'],
+            '3': ['d', 'e', 'f'],
+            '4': ['g', 'h', 'i'],
+            '5': ['j', 'k', 'l'],
+            '6': ['m', 'n', 'o'],
+            '7': ['p', 'q', 'r', 's'],
+            '8': ['t', 'u', 'v'],
+            '9': ['w', 'x', 'y', 'z']
+        }
+        letter_lists = []
+        for ch in digits:
+            letter_lists.append(mappings[ch])
+            
+        while len(letter_lists) > 1:
+            l1 = letter_lists.pop()
+            l2 = letter_lists.pop()
+            combos = []
+            for i in l1:
+                for j in l2:
+                    combos.append(j + i)
+            letter_lists.append(combos)
+            
+        return [] if not letter_lists else letter_lists[0]
+        
