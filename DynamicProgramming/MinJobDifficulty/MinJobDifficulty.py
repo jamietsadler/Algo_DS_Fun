a90c95a239ba67864d3e37cada17ca7268c5c0ac
Jamie Sadler <jamietsadler@hotmail.co.uk>
2023-03-30 11:59:06 +0100

    Min job difficulty DP

--- DynamicProgramming/MinJobDifficulty/MinJobDifficulty.py
+++ DynamicProgramming/MinJobDifficulty/MinJobDifficulty.py
@@ -36,4 +36,27 @@ class Solution:
             return res
 
         return recurse(0, d)
+
+class Solution(object):
+    def minDifficulty(self, jobDifficulty, d):
+        """
+        :type jobDifficulty: List[int]
+        :type d: int
+        :rtype: int
+        """
+        n = len(jobDifficulty)
+        dp = [[float("inf")] * n + [0] for _ in range(d + 1)]
+
+        for days_remaining in range(1, d + 1):
+            for i in range(n - days_remaining + 1):
+                daily_max_job_diff = 0
+                for j in range(i+1, n - days_remaining + 2):
+                    daily_max_job_diff = max(daily_max_job_diff, jobDifficulty[j - 1])
+                    dp[days_remaining][i] = min(dp[days_remaining][i],
+                                                      daily_max_job_diff + dp[days_remaining - 1][j])
+
+        if dp[d][0] == float('inf'):
+            return -1
+        print(dp)
+        return dp[d][0]     
             
