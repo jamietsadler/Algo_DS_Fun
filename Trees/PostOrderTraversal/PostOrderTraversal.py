561128dfa6256eb21e2523778b12cc2ae089e63f
Jamie Sadler <jamietsadler@hotmail.co.uk>
2023-04-01 14:12:07 +0100

    Postorder traversal

--- Trees/PostOrderTraversal/PostOrderTraversal.py
+++ Trees/PostOrderTraversal/PostOrderTraversal.py
@@ -1,9 +1,25 @@
+from typing import Optional, TreeNode, List
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        if not root:
+            return None
+
+        def helper(node, res):
+            if node:
+                helper(node.left, res)
+                helper(node.right, res)
+                res.append(node.val)
+                
+        helper(root, res)
+        return res
+    
 class Solution(object):
     def postorderTraversal(self, root):
         """
@@ -25,3 +41,23 @@ class Solution(object):
                     stack.append((node.left, False))
 
         return output
+
+class Solution(object):
+    def postorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+
+        if not root:
+            return None
+        stack = [root]
+        output = []
+
+        while stack:
+            node = stack.pop()
+            if node:
+                output.append(node.val)
+                stack.append(node.right)
+                stack.append(node.left)
+        return output[::-1]            
