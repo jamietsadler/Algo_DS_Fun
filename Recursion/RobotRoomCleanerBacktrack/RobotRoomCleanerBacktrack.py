152b08d55b930c00f6941ecb6284a4482a9411bb
Jamie Sadler <jamietsadler@hotmail.co.uk>
2023-04-06 17:34:47 +0100

    Robot room cleaner using backtracking

---
+++ Recursion/RobotRoomCleanerBacktrack/RobotRoomCleanerBacktrack.py
@@ -0,0 +1,63 @@
+# """
+# This is the robot's control interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class Robot:
+#    def move(self):
+#        """
+#        Returns true if the cell in front is open and robot moves into the cell.
+#        Returns false if the cell in front is blocked and robot stays in the current cell.
+#        :rtype bool
+#        """
+#
+#    def turnLeft(self):
+#        """
+#        Robot will stay in the same cell after calling turnLeft/turnRight.
+#        Each turn will be 90 degrees.
+#        :rtype void
+#        """
+#
+#    def turnRight(self):
+#        """
+#        Robot will stay in the same cell after calling turnLeft/turnRight.
+#        Each turn will be 90 degrees.
+#        :rtype void
+#        """
+#
+#    def clean(self):
+#        """
+#        Clean the current cell.
+#        :rtype void
+#        """
+
+class Solution:
+    def cleanRoom(self, robot):
+        """
+        :type robot: Robot
+        :rtype: None
+        """
+        def go_back():
+            robot.turnRight()
+            robot.turnRight()
+            robot.move()
+            robot.turnRight()
+            robot.turnRight()
+
+        directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+        visited = set()
+
+        def backtrack(x, y):
+            visited.add((x, y))
+            robot.clean()
+
+            for row, col in directions:
+                i = x + row
+                j = y + col
+
+                if not (i, j) in visited and robot.move():
+                    backtrack(i, j)
+                    go_back()
+                robot.turnRight()
+
+        backtrack(0, 0)
+        
