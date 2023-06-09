44c3a4308ddbc2ba39789553bef948ed4bf010d6
Jamie Sadler <jamietsadler@hotmail.co.uk>
2023-06-09 20:12:30 +0100

    More permutation solutions

--- Recursion/Permutations/Permutations.py
+++ Recursion/Permutations/Permutations.py
@@ -17,4 +17,59 @@ class Solution:
 
         backtrack()
         return ans
+
+
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+        used = [False]*len(nums)
+
+        def backtrack(combo, res, used):
+            if len(combo) == len(nums):
+                res.append(combo[:])
+                return
             
+            for i in range(len(nums)):
+                if not used[i]:
+                    combo.append(nums[i])
+                    used[i] = True
+                    backtrack(combo, res, used)
+                    used[i] = False
+                    combo.pop()
+
+        backtrack([], res, used)
+        return res
+
+
+
+# unique combinations
+class Solution(object):
+    def permuteUnique(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+        used = [False]*len(nums)
+
+        def backtrack(combo, res, used):
+            if len(combo) == len(nums):
+                res.append(combo[:])
+                return
+
+            lookup = set()
+            for i in range(len(nums)):
+                if not used[i] and nums[i] not in lookup:
+                    combo.append(nums[i])
+                    used[i] = True
+                    backtrack(combo, res, used)
+                    used[i] = False
+                    combo.pop()
+                    lookup.add(nums[i])
+
+        backtrack([], res, used)
+        return res
