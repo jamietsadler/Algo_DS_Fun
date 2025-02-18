28f026dc8e7f1b7566451b7791a1f918d0abf37f
Jamie Sadler <jamietsadler@hotmail.co.uk>
2025-02-18 22:22:28 +0800

    decode ways with bottom up

--- DynamicProgramming/DecodeWays/DecodeWays.py
+++ DynamicProgramming/DecodeWays/DecodeWays.py
@@ -15,6 +15,25 @@ class Solution(object):
         return dp[len(s)]
 
 class Solution(object):
+    def numDecodings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        dp = [0] * (len(s)+1)
+        dp[0] = 1
+        dp[1] = 0 if s[0] == "0" else 1
+
+        for i in range(2, len(s)+1):
+            if s[i-1] == '0':
+                dp[i]= dp[i-1]
+                
+            if int(s[i-2:i]) <= 26 and int(s[i-2:i]) >= 10:
+                dp[i] += dp[i-2]
+        
+        return dp[len(s)]
+    
+class Solution(object):
     def __init__(self):
         self.memo = {}
 
