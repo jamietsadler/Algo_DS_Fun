1c241d61f04647f070f637dff187bfd356a92bef
Jamie Sadler <jamietsadler@hotmail.co.uk>
2025-02-18 22:31:01 +0800

    iterative

--- DynamicProgramming/DecodeWays/DecodeWays.py
+++ DynamicProgramming/DecodeWays/DecodeWays.py
@@ -1,3 +1,29 @@
+class Solution(object):
+    def numDecodings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if s[0] == '0':
+            return 0
+
+        first = 1
+        second = 1 
+
+        for i in range(1, len(s)):
+            third = 0
+            if s[i] != '0':
+                third = second
+
+            if int(s[i-1:i+1]) <= 26 and int(s[i-1:i+1]) >= 10:
+                third += first
+            
+            first = second
+            second = third
+
+        return second
+
+
 
 class Solution(object):
     def numDecodings(self, s):
@@ -25,7 +51,7 @@ class Solution(object):
         dp[1] = 0 if s[0] == "0" else 1
 
         for i in range(2, len(s)+1):
-            if s[i-1] == '0':
+            if s[i-1] != '0':
                 dp[i]= dp[i-1]
                 
             if int(s[i-2:i]) <= 26 and int(s[i-2:i]) >= 10:
