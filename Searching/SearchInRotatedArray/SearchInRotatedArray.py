2b4b654b0c6bdc1866e80a1b35b272ed9b2e50d2
Jamie Sadler <jamietsadler@hotmail.co.uk>
2025-02-22 20:07:40 +0800

    search rotated array

--- Searching/SearchInRotatedArray/SearchInRotatedArray.py
+++ Searching/SearchInRotatedArray/SearchInRotatedArray.py
@@ -1,4 +1,31 @@
 from typing import List
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        i = 0 
+        j = len(nums) - 1
+
+        while i < j:
+            if nums[i] > nums[j]:
+                i += 1
+            else:
+                j -=1 
+            
+        def binary_search(left, right):
+            if nums[left] == target:
+                return left
+            if left >= right:
+                return -1 
+            if nums[left] < target:
+                return binary_search(left+ 1, right)
+            else:
+                return binary_search(left, right - 1)
+
+        # lhs 
+        if (ans := binary_search(0, i)) != -1:
+            return ans
+        return binary_search(j, len(nums) - 1)
+
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         low = 0
