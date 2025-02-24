005b0b8c0facd4c7f1c3a753433eebcd338dd3ea
Jamie Sadler <jamietsadler@hotmail.co.uk>
2025-02-24 20:18:10 +0800

    combination sum 2

--- Recursion/CombinationSumBacktracking/CombinationSum.py
+++ Recursion/CombinationSumBacktracking/CombinationSum.py
@@ -22,3 +22,35 @@ class Solution:
         helper([], target, 0)
 
         return res
+    
+class Solution2(object):
+    def combinationSum2(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        res = []
+
+        def helper(rem, combination, start):
+            if rem == 0:
+                res.append(combination[:])
+                return
+
+            if rem < 0:
+                return 
+
+            for i in range(start, len(candidates)):
+                if i > start and candidates[i] == candidates[i-1]:
+                    continue
+                num = candidates[i]
+                if rem - num >= 0:
+                    combination.append(num)
+                    helper(rem - num, combination, i+1)
+                    combination.pop()
+
+            return 
+
+        candidates.sort()
+        helper(target, [], 0)
+        return res
