95187defd4e3a818c3f5bf0a5b0b93a6af07d6f2
Jamie Sadler <jamietsadler@hotmail.co.uk>
2025-02-26 20:39:48 +0800

    Number of queens

--- Recursion/NQueens/NQueens.py
+++ Recursion/NQueens/NQueens.py
@@ -28,3 +28,39 @@ class Solution:
             return solutions
         
         return backtrack(0, set(), set(), set())
+    
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        
+        def backtrack(row, cols, diaganols, antidiaganols, state):
+            if row == n:
+                final = []
+                for row in state:
+                    tmp = "".join(row)
+                    final.append(tmp)                   
+                res.append(final[:])
+                return 
+
+            for col in range(n):
+                diag = row - col
+                anti_diag = col + row
+                if col in cols or diag in diaganols or anti_diag in antidiaganols:
+                    continue
+                
+                state[row][col] = 'Q'
+                cols.add(col)
+                diaganols.add(diag)
+                antidiaganols.add(anti_diag)
+                backtrack(row + 1, cols, diaganols, antidiaganols, state)
+
+                state[row][col] = '.'
+                cols.remove(col)
+                diaganols.remove(diag)
+                antidiaganols.remove(anti_diag)
+        res = []
+                
+        board = [['.'] * n for _ in range(n)]
+        backtrack(0, set(), set(), set(),board)
+        return res
+
+        
