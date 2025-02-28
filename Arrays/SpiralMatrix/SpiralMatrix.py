3b0f7addb6bc12c053f8761f99f30382882e66db
Jamie Sadler <jamietsadler@hotmail.co.uk>
2025-02-28 16:07:01 +0800

    spiral matrix 2

--- Arrays/SpiralMatrix/SpiralMatrix.py
+++ Arrays/SpiralMatrix/SpiralMatrix.py
@@ -1,5 +1,30 @@
 from typing import List
 
+class Solution(object):
+    def generateMatrix(self, n):
+        """
+        :type n: int
+        :rtype: List[List[int]]
+        """
+        
+        matrix = [[0] * n for _ in range(n)]
+        row = 0
+        col = 0
+        dirs = [(0,1), (1, 0), (0, -1), (-1, 0)]
+        diridx = 0
+
+        for num in range(1, (n**2)+1):
+            print(row, col)
+            matrix[row][col] = num
+            i, j = dirs[diridx]
+
+            if row + i >= len(matrix) or row + i < 0 or col + j >= len(matrix[0]) or col + j < 0 or matrix[row + i][col + j] > 0:
+                diridx = (diridx + 1) % 4
+
+            i, j = dirs[diridx]
+            row, col = row + i, col + j
+        
+        return matrix
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         m, n = len(matrix), len(matrix[0]) # Initial possible number of steps
