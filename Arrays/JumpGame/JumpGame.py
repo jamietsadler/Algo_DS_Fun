259630e8ba756e0cc7739e6b933946bb730415cd
Jamie Sadler <jamietsadler@hotmail.co.uk>
2025-05-08 11:32:30 +0800

    Jump Game 1

--- Arrays/JumpGame/JumpGame.py
+++ Arrays/JumpGame/JumpGame.py
@@ -1,3 +1,20 @@
+from typing import List
+
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        curr_furthest = 0
+
+        for i in range(len(nums)-1):
+            curr_furthest = max(curr_furthest, i + nums[i])
+            if i == curr_furthest:
+                return False
+        return True
+
+
 class Solution:
     def jump(self, nums: List[int]) -> int:
 
